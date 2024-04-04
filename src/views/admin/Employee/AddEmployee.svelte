a5488923cbe0af2748c71415eb5ca234d129c9b6
rouhanfarooqui <rouhanfarooqui@hotmail.com>
2024-04-04 10:15:05 +0500

    Update AddEmployee.svelte

--- src/views/admin/Employee/AddEmployee.svelte
+++ src/views/admin/Employee/AddEmployee.svelte
@@ -60,30 +60,27 @@
 </script>
 
 <div class="relative min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg px-4 py-10">
-    <div class="left-section flex flex-col items-center"> <!-- Adjust this value according to your needs -->
-      <!-- svelte-ignore a11y-img-redundant-alt -->
+
+
+  <!-- <div class="left-section flex flex-col items-center">
       <img id="profile-image" src="{image}" alt="Default Image" style="max-width: 200px; max-height: 200px;" />
       <input type="file" accept="image/*" id="profile-pic" style="display: none" />
       <label for="profile-pic" class="bg-blueGray-600 text-white active:bg-blueGray-800 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none ease-linear transition-all duration-150 mt-4 cursor-pointer">Upload</label>
     </div>
 
-    <div class="divider"></div>
+    <div class="divider"></div> -->
+
+    <div class="ml-auto w-6/12">
 
    <!-- Filters Row 1 -->
   <div class="flex justify-between mb-4">
-    <!-- Filter by EmployeeID -->
-    <div>
-      <label for="filterEmployeeID">Employee ID:</label>
-      <br>
-      <input type="text" id="filterEmployeeID" name="filterEmployeeID" class="filter-input" placeholder="Employee ID" onchange="{() => applyFilters()}">
-    </div>
-    <!-- Filter by Name -->
+    <!-- Name -->
     <div>
       <label for="filterName">Name:</label>
       <br>
       <input type="text" id="filterName" name="filterName" class="filter-input" placeholder="Name" onchange="{() => applyFilters()}">
     </div>
-   <!-- Filter by Department -->
+   <!-- Mobile Number -->
    <div>
     <label for="filterDepartment">Mobile Number:</label>
     <br>
@@ -93,13 +90,13 @@
 
   <!-- Filters Row 2 -->
   <div class="flex justify-between mb-4">
-    <!-- Filter by Position -->
+    <!-- Location -->
     <div>
       <label for="filterPosition">Location:</label>
       <br>
       <input type="text" id="filterPosition" name="filterPosition" class="filter-input" placeholder="Location" onchange="{() => applyFilters()}">
     </div>
-    <!-- Filter by Experience -->
+    <!-- Department -->
     <div>
       <label for="filterExperience">Department:</label>
       <br>
@@ -111,39 +108,17 @@
         <option value="5+">5+ years</option>
       </select>
     </div>
-    <!-- Filter by Active -->
-    <div>
-      <label for="filterActive">Designation:</label>
-      <br>
-      <select id="filterActive" name="filterActive" class="filter-input" onchange="{() => applyFilters()}">
-        <option value="">All</option>
-        <option value="true">Yes</option>
-        <option value="false">No</option>
-      </select>
-    </div>
   </div>
 
     <!-- Filters Row 3 -->
     <div class="flex justify-between mb-4">
-      <!-- Filter by Position -->
+      <!-- Email -->
       <div>
         <label for="filterPosition">Email:</label>
         <br>
         <input type="text" id="filterPosition" name="filterPosition" class="filter-input" placeholder="Email" onchange="{() => applyFilters()}">
       </div>
-      <!-- Filter by Experience -->
-      <div>
-        <label for="filterExperience">Employee Types:</label>
-        <br>
-        <select id="filterExperience" name="filterExperience" class="filter-input" onchange="{() => applyFilters()}">
-          <option value="">All</option>
-          <option value="0-1">0-1 years</option>
-          <option value="1-3">1-3 years</option>
-          <option value="3-5">3-5 years</option>
-          <option value="5+">5+ years</option>
-        </select>
-      </div>
-      <!-- Filter by Active -->
+      <!-- Gender -->
       <div>
         <label for="filterActive">Gender:</label>
         <br>
@@ -154,4 +129,26 @@
         </select>
       </div>
     </div>
+
+    <!-- Filters Row 4 -->
+    <div class="flex justify-between mb-4">
+      <!-- EmployeeID -->
+    <div>
+      <label for="filterEmployeeID">Employee ID:</label>
+      <br>
+      <input type="text" id="filterEmployeeID" name="filterEmployeeID" class="filter-input" placeholder="Employee ID" onchange="{() => applyFilters()}">
+    </div>
+      <!-- Designation -->
+    <div>
+      <label for="filterActive">Designation:</label>
+      <br>
+      <select id="filterActive" name="filterActive" class="filter-input" onchange="{() => applyFilters()}">
+        <option value="">All</option>
+        <option value="true">Yes</option>
+        <option value="false">No</option>
+      </select>
+    </div>
+  </div>
+    </div>
+
 </div>
