8e4e38dea040c8b9b9ab509cee197f18141de762
rouhanfarooqui <rouhanfarooqui@hotmail.com>
2024-05-12 00:38:03 +0500

    .

--- src/views/admin/RoomBooking/Rooms.svelte
+++ src/views/admin/RoomBooking/Rooms.svelte
@@ -247,7 +247,7 @@
                                     <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="grid-password">
                                         Features
                                     </label>
-                                    <select id="features" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" multiple bind:value={features}>
+                                    <select id="features" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" bind:value={features}>
                                         <option value="TV">TV</option>
                                         <option value="Projector">Projector</option>
                                         <option value="Speakers">Speakers</option>
