8d13762156db123c43a0300c14c93a4dd1a5a23c
ahmed-tariq2311 <124722114+ahmed-tariq2311@users.noreply.github.com>
2024-05-31 22:48:18 +0500

    live attendance

--- src/views/admin/Dashboard/LiveAttendance.svelte
+++ src/views/admin/Dashboard/LiveAttendance.svelte
@@ -1,6 +1,6 @@
 <script>
   import { onMount, onDestroy } from 'svelte';
-  import { getAllAttendances } from '../../../services/api'; // Adjust the import according to your project structure
+  import { getAllAttendances } from '../../../services/api'; 
   import Pagination from "../../../components/Pagination/Pagination.svelte";
 
   export let color = "light";
@@ -12,10 +12,13 @@
   }
 
   async function fetchData() {
-    const currentDate = new Date().toISOString().split('T')[0]; // Get current date in YYYY-MM-DD format
+    // const currentDate = new Date().toISOString().split('T')[0]
+    const currentDate = new Date('2024-05-28')
 
     try {
       let data = await getAllAttendances(currentDate, currentDate);
+      // console.log(data)
+      // return;
       // Fix the image paths
       attendanceData = data.map(record => {
         if (record.imagePath) {
@@ -31,7 +34,7 @@
 
   onMount(() => {
     fetchData();
-    pollInterval = setInterval(fetchData, 60000); // Poll every 60 seconds
+    pollInterval = setInterval(fetchData, 15000); // Poll every 15 seconds
 
     return () => {
       clearInterval(pollInterval);
@@ -42,11 +45,17 @@
     clearInterval(pollInterval);
   });
 
+  function handleRefresh() {
+    console.log("Refresh button clicked");
+    fetchData(); 
+  }
+
   function handlePageChange(event) {
     console.log("Received page change:", event.detail.pageNumber); // Confirm event reception
     currentPage = event.detail.pageNumber;
   }
 </script>
+
 
 <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-xl rounded-lg {color === 'light' ? 'bg-white' : 'bg-red-800 text-white'}">
   <div class="rounded-t mb-0 px-4 py-10 border-0">
@@ -56,13 +65,12 @@
           Live Data
         </h3>
       </div>
-      <input type="checkbox" id="showImage" name="showImage" class="mr-4">
-      <label for="showImage" class="font-semibold mr-4">Show Image in Live Transactions</label>
       <button
         class="bg-blueGray-600 text-white active:bg-blueGray-800 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
         type="button"
+        on:click={handleRefresh}
       >
-        Save
+        Refresh
       </button>
     </div>
   </div>
@@ -74,31 +82,16 @@
             Photo
           </th>
           <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left {color === 'light' ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100' : 'bg-red-700 custom-text border-red-600'}">
-            ID
+            ID                               
           </th>
           <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left {color === 'light' ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100' : 'bg-red-700 custom-text border-red-600'}">
             Name
           </th>
           <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left {color === 'light' ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100' : 'bg-red-700 custom-text border-red-600'}">
-            DateTime
+            Date
           </th>
           <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left {color === 'light' ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100' : 'bg-red-700 custom-text border-red-600'}">
             Status
-          </th>
-          <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left {color === 'light' ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100' : 'bg-red-700 custom-text border-red-600'}">
-            Verification Type
-          </th>
-          <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left {color === 'light' ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100' : 'bg-red-700 custom-text border-red-600'}">
-            Device
-          </th>
-          <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left {color === 'light' ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100' : 'bg-red-700 custom-text border-red-600'}">
-            Time Zone
-          </th>
-          <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left {color === 'light' ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100' : 'bg-red-700 custom-text border-red-600'}">
-            Finger Used 
-          </th>
-          <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left {color === 'light' ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100' : 'bg-red-700 custom-text border-red-600'}">
-            Status From Device
           </th>
         </tr>
       </thead>
@@ -106,15 +99,13 @@
         {#each attendanceData as record}
           <tr>
             <td class="table-data text-blueGray-600 text-xs">
-              <img src={record.imagePath || 'placeholder.jpg'} alt="Employee Photo" class="w-8 h-8 rounded-full"/>
+              <img src={record.photo || 'placeholder.jpg'} alt="Employee Photo" class="w-8 h-8 rounded-full"/>
             </td>
             <td class="table-data text-blueGray-600 text-xs" title={record.employee.employeeID}>{record.employee.employeeID}</td>
             <td class="table-data font-bold text-blueGray-600 text-xs" title={record.employee.name}>{record.employee.name}</td>
-            <td class="table-data text-blueGray-600 text-xs" title={record.date}>{new Date(record.date).toLocaleString()}</td>
+            <td class="table-data text-blueGray-600 text-xs" title={record.date}>{record.date}</td>
             <td class="table-data text-blueGray-600 text-xs" title={record.attendance}>{record.attendance}</td>
-            <td class="table-data text-blueGray-600 text-xs" title={record.device}>{record.device}</td>
-            <td class="table-data text-blueGray-600 text-xs" title={record.employee.fingerIndex1}>{record.employee.fingerIndex1}</td>
-            <td class="table-data text-blueGray-600 text-xs" title={record.statusFromDevice}>{record.statusFromDevice}</td>
+
           </tr>
         {/each}
       </tbody>
